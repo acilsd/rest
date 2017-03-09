90d639fe53f8c86dd3aa307f5bf8d176e9bf43a0
acilsd <acilsd@derpy.ru>
2017-03-09 22:10:37 +0300

    tweaked main component to work correctly (i hope)

--- client/App/index.js
+++ client/App/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import normalize from '../../node_modules/node-normalize-scss/_normalize.scss';
 
-const App = () => {
-  return (
-    <h1>Hello worild</h1>
-  );
-};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      workers: []
+    };
+  }
 
-export default App;
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const lng = '';
+    const lat = '';
+    fetch(`/api/workers?lng=${lng}&lat=${lat}`)
+      .then(data => data.json())
+      .then(json => this.setState({ workers: json }));
+  }
+
+  render() {
+    const { workers } = this.state;
+
+    return (
+      <div>
+        Hello world
+        <div ref={c => this.placeholder = c}></div>
+      </div>
+    );
+  }
+}
