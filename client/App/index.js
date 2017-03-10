9886c55696cbdc2255e172d29f5dbf0247b1a3bf
acilsd <acilsd@derpy.ru>
2017-03-10 17:40:24 +0300

    finished clientside placeholder

--- client/App/index.js
+++ client/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import normalize from '../../node_modules/node-normalize-scss/_normalize.scss';
+import styles from './style.css';
 
 export default class App extends Component {
   constructor() {
@@ -11,11 +12,16 @@ export default class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const lng = '';
-    const lat = '';
-    fetch(`/api/workers?lng=${lng}&lat=${lat}`)
+    const lng = this.lng.value;
+    const lat = this.lat.value;
+    fetch('/api/workers?lng=' + lng + '&lat=' + lat)
       .then(data => data.json())
-      .then(json => this.setState({ workers: json }));
+      .then(json => {
+        this.setState({
+          workers: json
+        });
+        //console.log(json);
+      });
   }
 
   render() {
@@ -23,8 +29,27 @@ export default class App extends Component {
 
     return (
       <div>
-        Hello world
-        <div ref={c => this.placeholder = c}></div>
+        <form id="search" onSubmit={this.handleSubmit}>
+          <label>Enter your Latitude:</label>
+          <input type="text" ref={c => this.lat = c} placeholder="latitude" required/>
+          <label>Enter your Longitude:</label>
+          <input type="text" ref={c => this.lng = c} placeholder="longitude" required/>
+          <input type="submit" value="Find Workers" />
+        </form>
+        <ul>
+          {
+            workers.map((worker, idx) => {
+              return (
+                <li key={idx}>
+                  <p>{worker.obj.available}</p>
+                  <p>{worker.obj.name}</p>
+                  <p>{worker.obj.rank}</p>
+                  <p>{Math.floor(worker.dis / 1000)} km</p>
+                </li>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
